aaeb8bce9ede0d63b61ea4c42db623fbfe361351
Giorgi Gogichaishvili <gogichaishvilig544@gmail.com>
2020-12-22 22:37:03 +0400

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Giorgi Gogichaishvili on 10/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let myView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        myView.backgroundColor = .red
+        myView.center = view.center
+        view.addSubview(myView)
+        
+        let button = UIButton(frame: CGRect(x: (view.frame.size.width-200)/2, y: view.frame.size.height - 220, width: 200, height: 70))
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Start", for: .normal)
+        button.addTarget(self, action: #selector(animate), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    @objc func animate() {
+//        UIView.animate(withDuration: 1, animations: {
+//            self.myView.alpha = 0.0
+//            self.myView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+//            self.myView.center = self.view.center
+            
+//        })
+        UIView.animate(withDuration: 1, animations: {
+            self.myView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+            self.myView.center = self.view.center
+        }, completion:  { done in
+            if done {
+                self.shrink()
+            }
+        })
+    }
+    func shrink() {
+        UIView.animate(withDuration: 1, animations: {
+            self.myView.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+            self.myView.center = self.view.center
+        }, completion: { done in
+            self.animate()
+        })
+    }
+}
+
